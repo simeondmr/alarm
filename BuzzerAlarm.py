fada9b3eccb8c639b39360a6bdae82da2b5ca132
simeondmr <dmrsim94@gmail.com>
2020-11-20 20:56:07 +0000

    Used buzzer alarm in light sensor

---
+++ BuzzerAlarm.py
@@ -0,0 +1,21 @@
+from time import sleep
+
+from RPi import GPIO
+
+
+class BuzzerAlarm:
+    DELAY = 0.0001
+    N_SOUND = 1000
+
+    def __init__(self, pin):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
+        self.pin = pin
+
+    def alarm_sound(self):
+        for i in range(0, self.N_SOUND):
+            GPIO.output(self.pin, GPIO.HIGH)
+            sleep(self.DELAY)
+            GPIO.output(self.pin, GPIO.LOW)
+            sleep(self.DELAY)
--- light_sensor.py
+++ light_sensor.py
@@ -1,27 +1,30 @@
 import RPi.GPIO as GPIO
 from threading import Lock
+
+from BuzzerAlarm import BuzzerAlarm
 from photoresistor import Photoresistor
 from time import sleep
 
 
 class LightSensor(Photoresistor):
-    CAPACITOR_DISCHARGE_TIME = 0.3
+    CAPACITOR_DISCHARGE_TIME = 0.4
     SAMPLING_DELAY = 1
+    APPROX_PRECISION = 500
 
     def __init__(self, pin):
         super().__init__(pin)
         self.mutex = Lock()
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
+        self.buzzer_alarm = BuzzerAlarm(35)
 
     def _trigger(self):
         current = self.read()
-        if self.low > current >= self.high:
-            #TODO: stop beep alarm
+        print("current: " + str(current+500) + " low: " + str(self.low) + " high: " + str(self.high))
+        if self.low > current + self.APPROX_PRECISION >= self.high and self.low > current - self.APPROX_PRECISION >= self.high:
             pass
         else:
-            #TODO: beep alarm
-            pass
+            self.buzzer_alarm.alarm_sound()
         sleep(self.SAMPLING_DELAY)
 
     def read(self):
